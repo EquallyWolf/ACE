2526419f96c1d23379b5a9f036222db168f77e81
EquallyWolf <shaiebilly+git@gmail.com>
2022-03-15 16:04:17 +0000

    Add passing tests

--- tests/test_ace.py
+++ tests/test_ace.py
@@ -1,7 +1,39 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from io import StringIO
 from ace import __version__
 import tomli
+
+
+@dataclass
+class Input(ABC):
+    prompt: str = ""
+
+    @abstractmethod
+    def get(self) -> str:
+        """Return an input from the user as a string, given a prompt."""
+        pass
+
+
+class TextInput(Input):
+    def get(self) -> str:
+        return input(f"{self.prompt} ")
 
 
 def test_version():
     with open("pyproject.toml", "rb") as f:
         assert __version__ == tomli.load(f)["tool"]["poetry"]["version"]
+
+
+def test_get_text_input_with_prompt(monkeypatch):
+    text_input = TextInput("Enter your name:")
+    assert text_input.prompt == "Enter your name:"
+    monkeypatch.setattr("sys.stdin", StringIO("John Doe"))
+    assert isinstance(text_input.get(), str), "Should return a string"
+
+
+def test_get_text_input_without_prompt(monkeypatch):
+    text_input = TextInput()
+    assert text_input.prompt == ""
+    monkeypatch.setattr("sys.stdin", StringIO("John Doe"))
+    assert isinstance(text_input.get(), str), "Should return a string"
