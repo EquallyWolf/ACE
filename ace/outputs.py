938bda991b2f09b69f98d5a92a3fefca33fdf203
EquallyWolf <shaiebilly+git@gmail.com>
2022-03-17 23:27:24 +0000

    Add Output base class

--- ace/outputs.py
+++ ace/outputs.py
@@ -1,3 +1,12 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+
+@dataclass
+class Output(ABC):
+    prefix: str = None
+
+
 class CommandLineOutput:
     def __init__(self, prefix: str = None):
         self.prefix = prefix
