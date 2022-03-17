05e50a4dcba5a24759f2d34564b4d4d569b08c9f
EquallyWolf <shaiebilly+git@gmail.com>
2022-03-17 10:55:25 +0000

    Provide docstrings for classes in inputs.py

--- ace/inputs.py
+++ ace/inputs.py
@@ -4,13 +4,45 @@ from dataclasses import dataclass
 
 @dataclass
 class Input(ABC):
+    """
+    The base class for all types of inputs.
+
+    Provides a common interface for interacting with the user
+    via any source of input.
+
+    Attributes
+    ----------
+    prompt: str
+        The prompt to display to the user.
+    """
+
     prompt: str = ""
 
     @abstractmethod
     def get(self) -> str:
-        """Return an input from the user as a string, given a prompt."""
+        """
+        Obtain the input from a user and return it as a string.
+        """
 
 
 class TextInput(Input):
+    """
+    Class for retrieving input from the user via a command line.
+
+    Attributes
+    ----------
+    prompt: str
+        The prompt to display to the user.
+    """
+
     def get(self) -> str:
+        """
+        Obtain the input from a user via the command line and
+        return it as a string.
+
+        Returns
+        -------
+        input: str
+            The input from the user.
+        """
         return input(f"{self.prompt} ")
