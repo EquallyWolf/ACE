fbe8943f2c7744163e239de78d8e4f9137c3f633
Illias Shaieb <shaiebilly+github@gmail.com>
2022-03-17 16:12:59 +0000

    User input setup (#2)
    The what
    =======
    This commit adds the Input base class and CommandLineInput class.
    
    The why
    =======
    The Input base class provides a common interface for interacting with the
    user via any source of input.
    
    The first concrete class added based on Input is the the CommandLineInput
    class, as the command line is a quick and easy way to get user input.

--- tests/test_ace.py
+++ tests/test_ace.py
@@ -1,7 +1,30 @@
-from ace import __version__
+from io import StringIO
+
 import tomli
+from ace import __version__
+from ace.inputs import CommandLineInput
 
 
 def test_version():
     with open("pyproject.toml", "rb") as f:
         assert __version__ == tomli.load(f)["tool"]["poetry"]["version"]
+
+
+def test_get_command_line_input_with_prompt(monkeypatch):
+    # Create a CommandLineInput object with a prompt
+    text_input = CommandLineInput("Enter your name:")
+    assert text_input.prompt == "Enter your name:"
+
+    # Mock user input to return "John Doe"
+    monkeypatch.setattr("sys.stdin", StringIO("John Doe"))
+    assert isinstance(text_input.get(), str), "Should return a string"
+
+
+def test_get_command_line_input_without_prompt(monkeypatch):
+    # Create a CommandLineInput object without a prompt
+    text_input = CommandLineInput()
+    assert text_input.prompt == ""
+
+    # Mock user input to return "John Doe"
+    monkeypatch.setattr("sys.stdin", StringIO("John Doe"))
+    assert isinstance(text_input.get(), str), "Should return a string"
