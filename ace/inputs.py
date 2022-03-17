adeaa8a61f01f749a9440ce02ec4f9d842df92ef
Illy Shaieb <shaiebilly+ace@gmail.com>
2022-03-17 16:12:59 +0000

    Add user input class
    Changes
    ---------
    
    This commit adds the Input base class and CommandLineInput class.
    
    Reasons
    ---------
    
    The Input base class provides a common interface for interacting with the
    user via any source of input.
    
    The first concrete class added based on Input is the the CommandLineInput
    class, as the command line is a quick and easy way to get user input.

---
+++ ace/inputs.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+
+@dataclass
+class Input(ABC):
+    """
+    The base class for all types of inputs.
+
+    Provides a common interface for interacting with the user
+    via any source of input.
+
+    Attributes
+    ----------
+    prompt: str
+        The prompt to display to the user.
+    """
+
+    prompt: str = ""
+
+    @abstractmethod
+    def get(self) -> str:
+        """
+        Obtain the input from a user and return it as a string.
+        """
+
+
+class CommandLineInput(Input):
+    """
+    Class for retrieving input from the user via a command line.
+
+    Attributes
+    ----------
+    prompt: str
+        The prompt to display to the user.
+    """
+
+    def get(self) -> str:
+        """
+        Obtain the input from a user via the command line and
+        return it as a string.
+
+        Returns
+        -------
+        input: str
+            The input from the user.
+        """
+        return input(f"{self.prompt} ")
