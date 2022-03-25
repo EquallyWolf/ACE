ba9b4f691b0463336b58cac8c0dbac4a5bb19855
Illy Shaieb <shaiebilly+ace@gmail.com>
2022-03-25 00:58:26 +0000

    Create main loop
    Changes
    ---------
    This commit introduces the main loop and a predict function.
    
    Reasons
    ---------
    The main loop provides the entry point to the application, which
    combines the inputs and outputs classes.

---
+++ main.py
@@ -0,0 +1,27 @@
+from ace.inputs import CommandLineInput
+from ace.outputs import CommandLineOutput
+
+
+def predict(text: str) -> str:
+    """
+    Predict the intent of the given text.
+
+    returns: The predicted intent.
+    """
+    return "unknown"
+
+
+def main() -> None:
+    """
+    The entry point of the program.
+    """
+    user_input = CommandLineInput("You:")
+    ace_output = CommandLineOutput("ACE:")
+
+    while True:
+        prediction = predict(user_input.get())
+        ace_output.broadcast(prediction)
+
+
+if __name__ == "__main__":
+    main()
