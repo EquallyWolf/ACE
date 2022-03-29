58301913838f00fbf02e667b7dbe338ed97cd929
IllyShaieb <shaiebilly+ace@gmail.com>
2022-03-29 08:37:29 +0100

    Add default return value to predict function docstring

--- ace/net.py
+++ ace/net.py
@@ -2,6 +2,6 @@ def predict(text: str) -> str:
     """
     Predict the intent of the given text.
 
-    returns: The predicted intent.
+    returns: The predicted intent, or "unknown" if the intent is unknown.
     """
     return "unknown"
