9ef41afd278fe5c332e6ac61de1835b2177cdb64
Illy Shaieb <shaiebilly+ace@gmail.com>
2022-03-29 12:27:28 +0100

    Refactor predict function
    Changes
    ---------
    This commit moves the predict function to net.py and adds the default
    return value to the predict function's docstring.
    
    It also adds a unit test for the predict function.
    
    Reasons
    ---------
    The movement of the predict function and addition of unit tests will allow
    for easy addition of other intents.

---
+++ ace/net.py
@@ -0,0 +1,7 @@
+def predict(text: str) -> str:
+    """
+    Predict the intent of the given text.
+
+    returns: The predicted intent, or "unknown" if the intent is unknown.
+    """
+    return "unknown"
