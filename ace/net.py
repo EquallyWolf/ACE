909bff5fb686affd21000e5b022f0a2b0945375a
IllyShaieb <shaiebilly+git@gmail.com>
2022-04-01 15:51:01 +0100

    Update predict function to use regex in net.py

--- ace/net.py
+++ ace/net.py
@@ -1,12 +1,14 @@
+import re
+
+
 def predict(text: str) -> str:
     """
     Predict the intent of the given text.
 
     returns: The predicted intent, or "unknown" if the intent is unknown.
     """
-    if text is None or not text.strip():
-        return "unknown"
-    if any(
-        word.lower() for word in text.split() if word.lower() in {"hello", "hi", "hey"}
+    if re.match(
+        "|".join(["hello", "hi", "hey", "hi there"]), text or "", re.IGNORECASE
     ):
         return "greeting"
+    return "unknown"
