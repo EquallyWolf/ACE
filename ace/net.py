cc89a98d78563ee239f357e0165fd9d6ce17f5bd
Illy Shaieb <shaiebilly+ace@gmail.com>
2022-04-05 15:44:32 +0100

    Move predict function into new IntentModel class

--- ace/net.py
+++ ace/net.py
@@ -1,26 +1,44 @@
 import re
 
-GREETINGS = ["hello", "hi", "hey", "hi there"]
-GOODBYES = ["goodbye", "good bye", "bye"]
 
-
-def predict(text: str) -> str:
+class IntentModel:
     """
-    Predict the intent of the given text.
-
-    returns: The predicted intent, or "unknown" if the intent is unknown.
+    A model that predicts the intent of the given text.
     """
-    if _is_match(text, GREETINGS):
-        return "greeting"
-    if _is_match(text, GOODBYES):
-        return "goodbye"
-    return "unknown"
 
+    def __init__(self) -> None:
+        self.intents = self._load_intents()
 
-def _is_match(text: str, patterns: str) -> bool:
-    """
-    Helper function to check if the given text matches the given pattern.
+    def predict(self, text: str) -> str:
+        """
+        Predict the intent of the given text.
 
-    returns: True if the text matches the pattern, False otherwise.
-    """
-    return bool(re.match("|".join(patterns), text or "", re.IGNORECASE))
+        returns: The predicted intent, or "unknown" if the intent is unknown.
+        """
+        return next(
+            (
+                intent
+                for intent, pattern in self.intents.items()
+                if re.search(pattern, text, re.IGNORECASE)
+            ),
+            "unknown",
+        )
+
+    def _is_match(self, text: str, patterns: str) -> bool:
+        """
+        Helper function to check if the given text matches the given pattern.
+
+        returns: True if the text matches the pattern, False otherwise.
+        """
+        return bool(re.match("|".join(patterns), text or "", re.IGNORECASE))
+
+    def _load_intents(self) -> dict[str, list]:
+        """
+        Helper function to create the intents dictionary.
+
+        returns: A dictionary of intents.
+        """
+        return {
+            "GREETINGS": ["hello", "hi", "hey", "hi there"],
+            "GOODBYES": ["goodbye", "good bye", "bye"],
+        }
