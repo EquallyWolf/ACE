4e022975990ffabf4734a08a5cdf92c70ccf50f4
Illy Shaieb <shaiebilly+ace@gmail.com>
2022-04-23 21:44:31 +0100

    Split the parametised tests for inputs in test_inputs.py

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -4,24 +4,61 @@ import pytest
 from ace.inputs import CommandLineInput
 
 
-@pytest.mark.parametrize(
-    "prompt,expected",
-    [
-        ("Enter your name", "Enter your name "),
-        ("Enter your age:", "Enter your age: "),
-        ("Email>", "Email> "),
-        ("", ""),
-        ("  ", ""),
-        (None, ""),
-        ("1.  ", "1. "),
-        ("Select menu number: ", "Select menu number: "),
-    ],
-)
-def test_get_command_line_input(prompt, expected, monkeypatch, capsys):
-    text_input = CommandLineInput(prompt)
-    monkeypatch.setattr("sys.stdin", StringIO("ABC"))
+class TestCommandLineInputs:
+    @pytest.mark.parametrize(
+        "prompt,expected",
+        [
+            ("", ""),
+            ("  ", ""),
+            (None, ""),
+        ],
+    )
+    def test_get_command_line_input_no_prompt(
+        self, prompt, expected, monkeypatch, capsys
+    ):
+        text_input = CommandLineInput(prompt)
+        monkeypatch.setattr("sys.stdin", StringIO("ABC"))
 
-    output = text_input.get()
+        output = text_input.get()
 
-    assert capsys.readouterr().out == expected
-    assert isinstance(output, str), "Should return a string"
+        assert capsys.readouterr().out == expected
+        assert isinstance(output, str), "Should return a string"
+
+    @pytest.mark.parametrize(
+        "prompt,expected",
+        [
+            ("A", "A "),
+            ("Enter age ", "Enter age "),
+            ("mobile number     ", "mobile number "),
+        ],
+    )
+    def test_get_command_line_input_no_special_character(
+        self, prompt, expected, monkeypatch, capsys
+    ):
+        text_input = CommandLineInput(prompt)
+        monkeypatch.setattr("sys.stdin", StringIO("ABC"))
+
+        output = text_input.get()
+
+        assert capsys.readouterr().out == expected
+        assert isinstance(output, str), "Should return a string"
+
+    @pytest.mark.parametrize(
+        "prompt,expected",
+        [
+            ("1.  ", "1. "),
+            ("Select menu number: ", "Select menu number: "),
+            ("What is your name?", "What is your name? "),
+            ("Email>", "Email> "),
+        ],
+    )
+    def test_get_command_line_input_special_character(
+        self, prompt, expected, monkeypatch, capsys
+    ):
+        text_input = CommandLineInput(prompt)
+        monkeypatch.setattr("sys.stdin", StringIO("ABC"))
+
+        output = text_input.get()
+
+        assert capsys.readouterr().out == expected
+        assert isinstance(output, str), "Should return a string"
