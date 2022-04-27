48ae6d0362231e02c2db65ba993549137c27eb02
Illy Shaieb <shaiebilly+ace@gmail.com>
2022-04-27 11:33:29 +0100

    Create function for open_app intent in main.py

--- main.py
+++ main.py
@@ -30,29 +30,7 @@ def main() -> None:
             case "goodbye":
                 goodbye(ace_output)
             case "open_app":
-                app_name = " ".join(text.split(" ")[1:])
-
-                with open(os.path.join("config", "apps.toml"), "rb") as f:
-                    app_data = tomli.load(f)
-
-                for app_id, aliases in app_data["aliases"].items():
-                    if app_name in aliases:
-                        app_name = app_id
-                        break
-
-                match platform.system().lower():
-                    case "windows":
-                        try:
-                            windowsapps.open_app(app_name)
-                            ace_output.broadcast(f"Opening '{app_name}'...")
-                        except FileNotFoundError:
-                            ace_output.broadcast(
-                                f"Sorry, I can't open '{app_name}'. Is it installed?"
-                            )
-                    case _:
-                        ace_output.broadcast(
-                            f"Sorry, I don't know how to open '{app_name}' on this platform."
-                        )
+                open_app(ace_output, text)
 
 
 def unknown(output: Output) -> None:
@@ -66,6 +44,30 @@ def greeting(output: Output) -> None:
 def goodbye(output: Output) -> None:
     output.broadcast("Goodbye!")
     quit()
+
+
+def open_app(output: Output, text: str) -> None:
+    app_name = " ".join(text.split(" ")[1:])
+
+    with open(os.path.join("config", "apps.toml"), "rb") as f:
+        app_data = tomli.load(f)
+
+    for app_id, aliases in app_data["aliases"].items():
+        if app_name in aliases:
+            app_name = app_id
+            break
+
+    match platform.system().lower():
+        case "windows":
+            try:
+                windowsapps.open_app(app_name)
+                output.broadcast(f"Opening '{app_name}'...")
+            except FileNotFoundError:
+                output.broadcast(f"Sorry, I can't open '{app_name}'. Is it installed?")
+        case _:
+            output.broadcast(
+                f"Sorry, I don't know how to open '{app_name}' on this platform."
+            )
 
 
 if __name__ == "__main__":
