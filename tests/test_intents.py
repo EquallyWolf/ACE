f209935329ce2c263dc15481f1d368dccdb7a290
Illy Shaieb <shaiebilly+ace@gmail.com>
2022-06-06 11:05:29 +0100

    Refactor test_intents.py to only test open_app on Windows

--- tests/test_intents.py
+++ tests/test_intents.py
@@ -1,3 +1,5 @@
+from platform import system
+from pytest import mark
 from ace import intents
 
 
@@ -19,36 +21,40 @@ def test_intent_goodbye():
     assert response == "Goodbye!"
 
 
-def test_intent_open_app_windows(monkeypatch):
-    monkeypatch.setattr("platform.system", lambda: "Windows")
-    monkeypatch.setattr("ace.application.os.popen", lambda x: None)
+@mark.skipif(system() != "Windows", reason="Windows-only tests")
+class TestOpenAppWindows:
+    @staticmethod
+    def test_intent_open_app_windows(monkeypatch):
+        monkeypatch.setattr("ace.application.os.popen", lambda x: None)
 
-    response = intents.open_app("open chrome")
+        response = intents.open_app("open chrome")
 
-    assert response == "Opening 'Chrome'..."
+        assert response == "Opening 'Chrome'..."
 
+    @staticmethod
+    def test_intent_open_app_unknown_platform(monkeypatch):
+        monkeypatch.setattr("platform.system", lambda: "ABC123")
+        monkeypatch.setattr("ace.application.os.popen", lambda x: None)
 
-def test_intent_open_app_unknown_platform(monkeypatch):
-    monkeypatch.setattr("platform.system", lambda: "ABC123")
-    monkeypatch.setattr("ace.application.os.popen", lambda x: None)
+        response = intents.open_app("open chrome")
 
-    response = intents.open_app("open chrome")
+        assert (
+            response
+            == "Sorry, I don't know how to open apps on this platform (ABC123)."
+        )
 
-    assert response == "Sorry, I don't know how to open apps on this platform (ABC123)."
+    @staticmethod
+    def test_intent_open_app_windows_not_installed(monkeypatch):
+        response = intents.open_app("open UnknownApp")
+        assert response == "Sorry, I can't open 'UnknownApp'. Is it installed?"
 
+    @staticmethod
+    def test_intent_open_app_windows_empty_app_list(monkeypatch):
+        monkeypatch.setattr("ace.application.os.popen", lambda x: None)
+        monkeypatch.setattr(
+            "ace.application.WindowsAppManager._find_apps", lambda x: None
+        )
 
-def test_intent_open_app_windows_not_installed(monkeypatch):
-    monkeypatch.setattr("platform.system", lambda: "Windows")
+        response = intents.open_app("open chrome")
 
-    response = intents.open_app("open UnknownApp")
-    assert response == "Sorry, I can't open 'UnknownApp'. Is it installed?"
-
-
-def test_intent_open_app_windows_empty_app_list(monkeypatch):
-    monkeypatch.setattr("platform.system", lambda: "Windows")
-    monkeypatch.setattr("ace.application.os.popen", lambda x: None)
-    monkeypatch.setattr("ace.application.WindowsAppManager._find_apps", lambda x: None)
-
-    response = intents.open_app("open chrome")
-
-    assert response == "Sorry, I can't open 'Chrome'. Is it installed?"
+        assert response == "Sorry, I can't open 'Chrome'. Is it installed?"
