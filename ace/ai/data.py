0efb5043e85ce888381e726ab73266ddc6b4ced2
Illy Shaieb <shaiebilly+ace@gmail.com>
2024-04-04 15:15:13 +0100

    Improve ai module
    Changes
    ---------
    This pull request substantially enhances the project's model training, dataset management, and 
    overall infrastructure. It introduces the ability to train and run models, augments the dataset for 
    improved quality, updates model configurations, and establishes Git LFS for handling large model 
    files. Additionally, it includes refactoring for better code organization, validation for data integrity, 
    and adjustments to terminology for consistency.
    
    Reasons
    ---------
    These changes collectively aim to improve the quality and robustness of the artificial intelligence 
    models being developed.  They streamline the development process, enhance dataset quality, 
    and prepare the project for handling larger, more complex models. It also allows for a faster
    generation of datasets.

--- ace/ai/data.py
+++ ace/ai/data.py
@@ -9,8 +9,14 @@ IntentClassifierDataset:
 #### Functions: None
 """
 
+import itertools
 from pathlib import Path
+import re
+import csv
+from typing import Union
+
 import pandas as pd
+from tqdm import tqdm
 
 from ace.utils import Logger
 
@@ -60,7 +66,7 @@ class IntentClassifierDataset:
 
     def __getitem__(self, index: int) -> tuple[str, str]:
         """
-        Get a tuple of the text and intent at the given index.
+        Get a tuple of the phrase and intent at the given index.
 
         #### Parameters:
 
@@ -68,11 +74,12 @@ class IntentClassifierDataset:
             The index of the data to get.
 
         #### Returns: tuple[str, str]
-            A tuple of the text and intent at the given index.
+            A tuple of the phrase and intent at the given index.
 
         #### Raises: None
         """
-        return tuple(self.data.iloc[index])
+        result = self.data.iloc[index]
+        return result["phrase"], result["intent"]
 
     @property
     def intents(self) -> list:
@@ -129,3 +136,220 @@ class IntentClassifierDataset:
         if shuffle:
             data = data.sample(frac=1, random_state=self._seed)
         return data
+
+
+def load_entities(entities_directory: str = "data/rules/entities") -> dict:
+    """
+    Load the entities into a dictionary from the entities files.
+
+    #### Parameters:
+
+    entities_directory: str (default: "data/rules/entities")
+        The directory containing the entity files.
+
+    #### Returns: dict
+        A dictionary of the entities and their values.
+
+    #### Raises: FileNotFoundError
+        If no entities are found in the given directory.
+    """
+    entities_dir = Path(entities_directory)
+
+    with logger.log_context(
+        "info",
+        f"Loading raw entities dictionary from: {entities_dir}",
+        "Finished creating raw entities",
+    ):
+        entities = {}
+        for entity in entities_dir.glob("*.entity"):
+            logger.log("debug", f"Loading entity: {entity}")
+
+            content = entity.read_text().splitlines()
+
+            if not content:
+                logger.log("warning", f"Skipping empty entity file: {entity}")
+                continue
+
+            entities[entity.stem] = content
+
+        # If entities is empty, then we have no entities
+        if not entities:
+            logger.log("fatal", "No entities found.")
+            raise FileNotFoundError(f"No entities found in directory '{entities_dir}'.")
+
+        return entities
+
+
+def load_intents(intents_directory: str = "data/rules/intents") -> dict:
+    """
+    Load the intents into a dictionary from the intents files.
+
+    #### Parameters:
+
+    intents_directory: str (default: "data/rules/intents")
+        The directory containing the intent files.
+
+    #### Returns: dict
+        A dictionary of the intents and their values.
+
+    #### Raises: FileNotFoundError
+        If no intents are found in the given directory.
+    """
+    intents_dir = Path(intents_directory)
+
+    with logger.log_context(
+        "info",
+        f"Loading raw intents dictionary from: {intents_dir}",
+        "Finished creating raw intents",
+    ):
+        intents = {
+            intent.stem: intent.read_text().splitlines()
+            for intent in intents_dir.glob("*.intent")
+        }
+
+        # If intents is empty, then we have no intents
+        if not intents:
+            logger.log("fatal", "No intents found.")
+            raise FileNotFoundError(f"No intents found in directory '{intents_dir}'.")
+
+    return intents
+
+
+def generate_intent_dataset(
+    raw_intents: dict, raw_entities: dict, num_examples: int = 100
+) -> dict[str, Union[set[str], list[str]]]:
+    """
+    Generates all combinations of intents and entities, but only if the entity is in the intent.
+
+    #### Parameters:
+
+    raw_intents: dict
+        A dictionary of the intents and their values.
+
+    raw_entities: dict
+        A dictionary of the entities and their values.
+
+    num_examples: int (default: 100)
+        The number of examples to generate for each intent.
+
+    #### Returns: dict[str, Union[set[str], list[str]]
+        The generated dataset.
+            format: {intent: {example1, example2, ...}}
+                    {intent: [example1, example2, ...]}
+
+    #### Raises: None
+    """
+
+    dataset = {}
+    for intent, intent_templates in tqdm(raw_intents.items(), desc="Creating dataset"):
+        logger.log("info", f"Creating dataset for intent '{intent}'.")
+
+        examples = []
+        for template in intent_templates:
+            logger.log("debug", f"Generating examples for template: {template}")
+
+            # Check if we have any entities in the template
+            entities = re.findall(r"{(.*?)}", template)
+            logger.log("debug", f"Entities in template: {entities}")
+
+            if not entities:
+                examples.append(template)
+                continue
+
+            # Generate all possible combinations of entities
+            try:
+                examples = list(
+                    map(
+                        lambda combination: template.format(
+                            **dict(zip(entities, combination))
+                        ),
+                        itertools.product(
+                            *[raw_entities[entity] for entity in entities]
+                        ),
+                    )
+                )
+            except KeyError as e:
+                logger.log("warning", f"No entity examples found for: {e}")
+                continue
+
+        dataset[intent] = set(examples[:num_examples])
+
+    # Check if we have any examples
+    if not dataset:
+        logger.log("critical", "No examples generated.")
+        raise ValueError("No examples generated.")
+
+    logger.log("info", f"Dataset: {dataset}")
+
+    return dataset
+
+
+def save_dataset(
+    dataset: dict[str, Union[set[str], list[str]]],
+    directory: str,
+    filename: str = "dataset.csv",
+) -> None:
+    """
+    Save the dataset to the given format.
+
+    #### Parameters:
+
+    dataset: dict[str, Union[set[str], list[str]]]
+        The dataset to save.
+
+    directory: str
+        The directory to save the dataset to.
+
+    filename: str (default: "dataset.csv")
+        The name of the file to save the dataset to.
+
+    #### Returns: None
+
+    #### Raises: ValueError
+        If the file type is not supported.
+    """
+    root_dir = Path(directory)
+    root_dir.mkdir(parents=True, exist_ok=True)
+
+    save_path = root_dir / filename
+
+    handlers = {
+        ".csv": _save_as_csv,
+    }
+
+    with logger.log_context(
+        "info",
+        f"Saving dataset to: {save_path}",
+        "Finished saving dataset.",
+    ):
+        if handler := handlers.get(save_path.suffix):
+            handler(dataset, save_path)
+        else:
+            logger.log("critical", f"Unsupported file type: {save_path.suffix}")
+            raise ValueError(f"Unsupported file type: {save_path.suffix}")
+
+
+def _save_as_csv(
+    dataset: dict[str, Union[set[str], list[str]]], save_path: Path
+) -> None:
+    """
+    Save the dataset as a CSV file.
+
+    #### Parameters:
+
+    dataset: dict[str, Union[set[str], list[str]]]
+        The dataset to save.
+
+    save_path: Path
+        The path to save the dataset to.
+
+    #### Returns: None
+
+    #### Raises: None
+    """
+    with open(save_path, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["phrase", "intent"])
+        for intent, examples in dataset.items():
+            for example in examples:
+                writer.writerow([example, intent])
--- tests/test_ai/test_data.py
+++ tests/test_ai/test_data.py
@@ -41,3 +41,159 @@ class TestIntentClassifierDataset:
 
         assert len(train_shuffled) == 8
         assert len(test_shuffled) == 2
+
+
+class TestLoadEntities:
+
+    def test_load_entities(self) -> None:
+        raw_entities = data.load_entities(
+            entities_directory="tests/data/rules/entities"
+        )
+
+        assert len(raw_entities) == 2
+
+        for entity in raw_entities:
+            assert entity in ["example_entity1", "example_entity2"]
+
+    def test_load_entities_invalid_directory(self) -> None:
+        with pytest.raises(FileNotFoundError):
+            data.load_entities(entities_directory="tests/data/rules/invalid")
+
+    def test_load_entities_empty_entity(self) -> None:
+        raw_entities = data.load_entities(
+            entities_directory="tests/data/rules/validations/empty"
+        )
+
+        assert len(raw_entities) == 1
+
+
+class TestLoadIntents:
+    def test_load_intents(self) -> None:
+        raw_intents = data.load_intents(intents_directory="tests/data/rules/intents")
+
+        assert len(raw_intents) == 2
+
+        for intent in raw_intents:
+            assert intent in ["example1", "example2"]
+
+    def test_load_intents_invalid_directory(self) -> None:
+        with pytest.raises(FileNotFoundError):
+            data.load_intents(intents_directory="tests/data/rules/invalid")
+
+
+class TestGenerateIntentDataset:
+
+    @pytest.mark.parametrize(
+        "raw_intents, raw_entities, num_examples, expected_dataset",
+        [
+            (  # One intent, no entities, one example
+                {"greet": ["hello"]},
+                {},
+                1,
+                {"greet": {"hello"}},
+            ),
+            (  # One intent, no entities, two examples
+                {"greet": ["hello"]},
+                {},
+                2,
+                {"greet": {"hello"}},
+            ),
+            (  # One intent, one entity, two examples
+                {"greet": ["hello {name}"]},
+                {"name": ["Alice", "Bob"]},
+                2,
+                {"greet": {"hello Alice", "hello Bob"}},
+            ),
+            (  # One intent, multiple entities, two examples
+                {"greet": ["hello {name} {age}"]},
+                {"name": ["Alice", "Bob"], "age": ["10", "20"]},
+                2,
+                {"greet": {"hello Alice 10", "hello Alice 20"}},
+            ),
+            (  # One intent, multiple entities, more examples than combinations
+                {"greet": ["hello {name} {age}"]},
+                {"name": ["Alice", "Bob"], "age": ["10", "20"]},
+                10,
+                {
+                    "greet": {
+                        "hello Alice 10",
+                        "hello Alice 20",
+                        "hello Bob 10",
+                        "hello Bob 20",
+                    }
+                },
+            ),
+            (  # Two intents, one entity, two examples
+                {"greet": ["hello {name}"], "goodbye": ["goodbye {name}"]},
+                {"name": ["Alice", "Bob"]},
+                2,
+                {
+                    "greet": {"hello Alice", "hello Bob"},
+                    "goodbye": {"goodbye Alice", "goodbye Bob"},
+                },
+            ),
+        ],
+    )
+    def test_generate_intent_dataset(
+        self, raw_intents, raw_entities, num_examples, expected_dataset
+    ) -> None:
+
+        dataset = data.generate_intent_dataset(raw_intents, raw_entities, num_examples)
+
+        assert dataset == expected_dataset
+
+    def test_generate_intent_dataset_no_data(self) -> None:
+        with pytest.raises(ValueError):
+            data.generate_intent_dataset(
+                raw_intents={}, raw_entities={}, num_examples=1
+            )
+
+
+class TestSaveDataset:
+    @pytest.mark.parametrize(
+        "dataset",
+        [
+            {
+                "intent1": ["example1", "example2"],
+                "intent2": ["example3", "example4"],
+            },
+            {
+                "intent1": {"example1", "example2"},
+                "intent2": {"example3", "example4"},
+            },
+        ],
+    )
+    def test_save_dataset(self, dataset) -> None:
+        save_dir = "tests/data/datasets"
+        save_filename = "test_dataset.csv"
+
+        data.save_dataset(dataset=dataset, directory=save_dir, filename=save_filename)
+
+        assert Path(f"{save_dir}/{save_filename}").exists()
+
+        Path(f"{save_dir}/{save_filename}").unlink()
+        Path(f"{save_dir}").rmdir()
+
+    @pytest.mark.parametrize(
+        "dataset",
+        [
+            {
+                "intent1": ["example1", "example2"],
+                "intent2": ["example3", "example4"],
+            },
+            {
+                "intent1": {"example1", "example2"},
+                "intent2": {"example3", "example4"},
+            },
+        ],
+    )
+    def test_invalid_file_type(self, dataset) -> None:
+        save_dir = "tests/data/datasets"
+        save_filename = "test_dataset.txt"
+
+        with pytest.raises(ValueError):
+            data.save_dataset(
+                dataset=dataset, directory=save_dir, filename=save_filename
+            )
+
+        assert not Path(f"{save_dir}/{save_filename}").exists()
